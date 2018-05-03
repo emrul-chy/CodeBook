8f54ede25b77f64d0da1e77be960a2c0f0c9dcc5
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-05-03 22:07:27 +0600

    Update Merge Sort Tree.cpp

--- Merge Sort Tree.cpp
+++ Merge Sort Tree.cpp
@@ -3,41 +3,33 @@
         Problem: Codeforces 91E
 */
 
-struct MergeSortTree {
-        vector <int> tree[4 * Max];
-        int ar[Max];
+vector <int> tree[4 * Max];
+int ar[Max];
 
-        MergeSortTree() {
-                for( int i=0; i<Max; i++ ) {
-                        tree[i].clear();
-                }
-        }
+void build( int node, int l, int r ) {
+	if( l == r ) {
+		tree[node].push_back( ar[l] );
+		return;
+	}
+	int mid = (l + r) >> 1;
+	int left = node * 2;
+	int right = node * 2 + 1;
+	build( left, l, mid );
+	build( right, mid + 1, r);
+	merge( tree[left].begin(), tree[left].end(), tree[right].begin(), tree[right].end(), back_inserter( tree[node] ) );
+}
 
-        void build( int node, int l, int r ) {
-                if( l == r ) {
-                        tree[node].push_back( ar[l] );
-                        return;
-                }
-                int mid = (l + r) >> 1;
-                int left = node * 2;
-                int right = node * 2 + 1;
-                build( left, l, mid );
-                build( right, mid + 1, r);
-                merge( tree[left].begin(), tree[left].end(), tree[right].begin(), tree[right].end(), back_inserter( tree[node] ) );
-        }
-
-        int query( int node, int l, int r, int L, int R, int val ) {
-                if( L > r || l > R ) {
-                        return 0;
-                }
-                if( L <= l && r <= R ) {
-                        return lower_bound( tree[node].begin(), tree[node].end(), val ) - tree[node].begin();
-                }
-                int mid = (l + r) >> 1;
-                int left = node * 2;
-                int right = node * 2 + 1;
-                int u = query( left, l, mid, L, R, val );
-                int v = query( right, mid+1, r, L, R, val );
-                return u + v;
-        }
-} mst;
+int query( int node, int l, int r, int L, int R, int val ) {
+	if( L > r || l > R ) {
+		return 0;
+	}
+	if( L <= l && r <= R ) {
+		return lower_bound( tree[node].begin(), tree[node].end(), val ) - tree[node].begin();
+	}
+	int mid = (l + r) >> 1;
+	int left = node * 2;
+	int right = node * 2 + 1;
+	int u = query( left, l, mid, L, R, val );
+	int v = query( right, mid+1, r, L, R, val );
+	return u + v;
+}
