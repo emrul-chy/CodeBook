8a93156fd120e95393a411aad6f3479376a885fa
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-05-10 18:07:41 +0600

    Create String Hashing.cpp

---
+++ String Hashing.cpp
@@ -0,0 +1,35 @@
+/*
+        String Hashing
+        */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long int ull ;
+const int base = 31 ;
+const int Max = 1e5;
+
+unordered_map < ull , ull > mp ;
+
+ull pw[Max];
+ull Hash[Max];
+
+void pre_power( ) {
+	pw[0] = 1 ;
+	for ( int i = 1; i < Max; i++ ) {
+		pw[i] = pw[i - 1] * base ;
+	}
+}
+
+ull Hashing( char str [] , int len ) {
+	ull hash_val = 0;
+	for ( int i = 0; i < len; i++ ) {
+		hash_val += str[i] * pw[i];
+		Hash[i] = hash_val;
+	}
+	return hash_val;
+}
+
+ull SubstringHash( int l, int r ) {
+	return Hash[r] - hash[l - 1] * pw[r - l + 1];
+}
+
