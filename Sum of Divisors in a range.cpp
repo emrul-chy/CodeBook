27c6eabe8a60ecbfc62d33eee4b96b60d0ffe41f
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 19:09:53 +0600

    Update Sum of Divisors in a range.cpp

--- Sum of Divisors in a range.cpp
+++ Sum of Divisors in a range.cpp
@@ -1,27 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
-ll triangle(ll a, ll b)
-{
-        return (a + b + 1) * (b - a) / 2 ;
+ll triangle(ll a, ll b) {
+	return (a + b + 1) * (b - a) / 2 ;
 }
 
-ll divSum(ll a, ll b) // Sum of divisors between a to b
-{
-        ll n = sqrt(b);
-        ll sum = 0;
+ll divSum(ll a, ll b) { // Sum of divisors between a to b
+	ll n = sqrt(b);
+	ll sum = 0;
 
-        for (ll i = 1; i <= n; i++)
-                sum += i * (b / i - a / i) + triangle(max(n, a / i), max(n, b / i));
-        return sum;
+	for(ll i = 1; i <= n; i++)
+		sum += i * (b / i - a / i) + triangle(max(n, a / i), max(n, b / i));
+	return sum;
 }
 
-int main()
-{
-        ll a, b;
-        cin >> a >> b;
-        ll ans = divSum(a - 1, b);
-        cout << ans << endl;
+int main() {
+	ll a, b;
+	cin >> a >> b;
+	ll ans = divSum(a - 1, b);
+	cout << ans << endl;
 
-        return 0;
+	return 0;
 }
