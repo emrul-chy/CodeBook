76810d38130ad769946627c1955aa1cdb38f26d6
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 19:09:09 +0600

    Update Modular Inverse.cpp

--- Modular Inverse.cpp
+++ Modular Inverse.cpp
@@ -1,11 +1,9 @@
 pll extendedEuclid(ll a, ll b) { // returns x, y | ax + by = gcd(a,b)
-
-	if (b == 0) return pll(1LL, 0LL);
+	if(b == 0) return pll(1LL, 0LL);
 	else {
 		pll d = extendedEuclid(b, a % b);
 		return pll(d.second, d.first - d.second * (a / b));
 	}
-
 }
 
 ll modularInverse(ll a, ll n) {
