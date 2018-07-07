45351133ed9f03624a44f57517e3b8125e5d82df
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 15:31:16 +0600

    Update Segment-Tree.cpp

--- Segment-Tree.cpp
+++ Segment-Tree.cpp
@@ -7,8 +7,8 @@ const int Max = 1e5 + 10;
 int ar[Max];
 int seg[Max * 4];
 
-void build( int node, int l, int r ) {
-	if( l == r ) {
+void build(int node, int l, int r) {
+	if(l == r) {
 		seg[node] = ar[l];
 		return;
 	}
@@ -20,26 +20,25 @@ void build( int node, int l, int r ) {
 	seg[node] = seg[lf] + seg[rt];
 }
 
-int update( int node, int l, int r, int idx, int val ) {
-	if( l == r ) {
+int update(int node, int l, int r, int idx, int val) {
+	if(l == r) {
 		seg[node] = val;
 		return seg[node];
 	}
-	int mid = (l + r) >> 1, u = seg[node*2], v = seg[node*2 + 1];
-	if( mid >= idx ) {
-		u = update(2*node, l, mid, idx, val);
-	}
-	else {
-		v = update(2*node+1, mid+1, r, idx, val);
+	int mid = (l + r) >> 1, u = seg[node * 2], v = seg[node * 2 + 1];
+	if(mid >= idx) {
+		u = update(2 * node, l, mid, idx, val);
+	} else {
+		v = update(2 * node + 1, mid + 1, r, idx, val);
 	}
 	return seg[node] = u + v;
 }
 
-int query( int node, int l, int r, int L, int R ) {
-	if( R < l || L > r) {
+int query(int node, int l, int r, int L, int R) {
+	if(R < l || L > r) {
 		return 0;
 	}
-	if( L <= l && r <= R ) {
+	if(L <= l && r <= R) {
 		return seg[node];
 	}
 	int mid = (l + r) >> 1;
