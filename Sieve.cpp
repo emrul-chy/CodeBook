f8a58cf32956e3d40141399df05c08d3a932adc1
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 15:24:13 +0600

    Update Sieve.cpp

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,18 +1,18 @@
 bool isPrime[1000101];
 
 void sieve() {
-        int MAX = 1000100, sq = sqrt(MAX);
-        memset(isPrime, true, sizeof isPrime);
-        for( int i=4; i<=MAX; i+=2) {
-                isPrime[i] = 0;
-        }
-        for( int i=3; i<=sq; i+=2 ) {
-                if( isPrime[i] ) {
-                        for( int j=i*i; j<=MAX; j+=i ){
-                                isPrime[j] = 0;
-                        }
-                }
-        }
-        isPrime[1] = 0;
-        isPrime[0] = 0;
+	int lim = 1000100, sq = sqrt(lim);
+	memset(isPrime, true, sizeof isPrime);
+	for(int i = 4; i <= lim; i += 2) {
+		isPrime[i] = 0;
+	}
+	for(int i = 3; i <= sq; i += 2) {
+		if(isPrime[i]) {
+			for(int j = i * i; j <= lim; j += i) {
+				isPrime[j] = 0;
+			}
+		}
+	}
+	isPrime[1] = 0;
+	isPrime[0] = 0;
 }
