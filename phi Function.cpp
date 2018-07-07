3d2183dab60f17321051bc53ccc60f11c6a09cba
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 19:08:18 +0600

    Update phi Function.cpp

--- phi Function.cpp
+++ phi Function.cpp
@@ -1,13 +1,13 @@
 ll phi[Max];
 
-void phigen( int n ) {
-	for ( int i=1; i<=n; i++ ) {
+void phigen(int n) {
+	for(int i = 1; i <= n; i++) {
 		phi[i] = i;
 	}
-	for ( int p=2; p<=n; p++) { 
-		if ( phi[p] == p ) {
+	for(int p = 2; p <= n; p++) {
+		if(phi[p] == p) {
 			phi[p] = p - 1;
-			for ( int i=2*p; i<=n; i+=p ) {
+			for(int i = 2 * p; i <= n; i += p) {
 				phi[i] = (phi[i] / p) * (p - 1);
 			}
 		}
