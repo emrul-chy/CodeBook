377509de70a77f362b442f7975474f813a6ee498
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 16:21:33 +0600

    Update Mo's Algorithm.cpp

--- Mo's Algorithm.cpp
+++ Mo's Algorithm.cpp
@@ -4,10 +4,10 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 typedef long long int ll;
 typedef pair <ll, ll> pll;
- 
+
 const int Max = 1e6 + 10;
 const int Mod = 1e9 + 7;
 const ll Inf = 1LL << 62;
@@ -20,101 +20,96 @@ ll cnt;
 int ar[Max];
 ll ans[Max];
 
-struct Node{
-      ll l, r, id;
+struct Node {
+	ll l, r, id;
 
-      Node () {};
+	Node() {};
 
-      Node (int l_, int r_, int id_) {
-            l = l_; r = r_; id = id_;
-      }
+	Node(int l_, int r_, int id_) {
+		l = l_;
+		r = r_;
+		id = id_;
+	}
 
-      bool operator<(const Node& b) const{
-            int block_a = l / BLOCK_SIZE;
-            int block_b = b.l / BLOCK_SIZE;
-            if( block_a != block_b ){
-                  return block_a < block_b;
-            } return r < b.r;
-      }
+	bool operator<(const Node &b) const {
+		if(l / BLOCK_SIZE < b.l / BLOCK_SIZE) {
+			return 1;
+		}
+		if(l / BLOCK_SIZE > b.l / BLOCK_SIZE) {
+			return 0;
+		}
+		return ((l / BLOCK_SIZE) & 1) ? r < b.r : r > b.r;
+	}
 } qr[Max];
 
 int tog[Max];
 
-void add( int x ) {
-      cnt -= mp[x] * mp[x] * x;
-      mp[x]++;
-      cnt += mp[x] * mp[x] * x;
+void add(int x) {
+	cnt -= mp[x] * mp[x] * x;
+	mp[x]++;
+	cnt += mp[x] * mp[x] * x;
 }
 
+void del(int x) {
+	cnt -= mp[x] * mp[x] * x;
+	mp[x]--;
+	cnt += mp[x] * mp[x] * x;
+}
 
-void del( int x ) {
-      cnt -= mp[x] * mp[x] * x;
-      mp[x]--;
-      cnt += mp[x] * mp[x] * x;
-}    
+int main() {
+#ifdef Mr_Emrul
+	freopen("inputf.in", "r", stdin);
+#endif ///Mr_Emrul
 
-int main(){
-      #ifdef Mr_Emrul
-            freopen("input.txt", "r", stdin);
-            freopen("output.txt", "w", stdout);
-      #endif /// Mr_Emrul
+	//ios_base::sync_with_stdio(false);
+	//cin.tie(0);
 
-      //ios_base::sync_with_stdio(false);
-      //cin.tie(0);
+	int n, m, l, r;
+	scanf("%d %d", &n, &m);
+	BLOCK_SIZE = 1000;
 
-      int n, m, l, r;
+	for(int i = 1; i <= n; i++) {
+		scanf("%d", &ar[i]);
+	}
 
+	for(int i = 1; i <= m; i++) {
+		scanf("%d %d", &l, &r);
+		qr[i] = Node(l, r, i);
+	}
 
-      while( scanf("%d %d", &n, &m) == 2 ){
-            BLOCK_SIZE = 1000;
-            memset(mp, 0, sizeof mp);
-            cnt = 0;
+	sort(qr + 1, qr + m + 1);
 
-            for( int i=1; i<=n; i++ ){
-                  scanf("%d", &ar[i]);
-            }
+	ll L = 1, R = 0;
 
-            for( int i=1; i<=m; i++ ){
-                  scanf("%d %d", &l, &r);
-                  qr[i] = Node(l, r, i);
-            }
+	for(int i = 1; i <= m; i++) {
+		ll l = qr[i].l;
+		ll r = qr[i].r;
 
-            sort( qr + 1, qr + m + 1 );
+		while(R < r) {
+			R++;
+			add(ar[R]);
+		}
 
-            ll L = 1, R = 0;
+		while(R > r) {
+			del(ar[R]);
+			R--;
+		}
 
-            for( int i=1; i<=m; i++ ){
-                  ll l = qr[i].l;
-                  ll r = qr[i].r;
+		while(L < l) {
+			del(ar[L]);
+			L++;
+		}
 
-                  while( R < r ){
-                        R++;
-                        add( ar[R] );
-                  }
+		while(L > l) {
+			L--;
+			add(ar[L]);
+		}
 
-                  while( R > r ){
-                        del( ar[R] );
-                        R--;
-                  }
+		ans[ qr[i].id ] = cnt;
+	}
 
-                  while( L < l ){
-                        del( ar[L] );
-                        L++; 
-                  }
-
-                  while( L > l ){
-                        L--;
-                        add( ar[L] );
-                  }
-
-                  ans[ qr[i].id ] = cnt;
-            }
-
-            for( int i=1; i<=m; i++ ){
-                  printf("%lld\n", ans[i]);
-            }
-
-            cerr << "--------------------" << '\n';
-      }   
-      return 0;
+	for(int i = 1; i <= m; i++) {
+		printf("%lld\n", ans[i]);
+	}
+	return 0;
 }
