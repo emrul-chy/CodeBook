9bdbc4910bc8e9459cb0ed85577388247b285c34
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 15:26:20 +0600

    Update Binary Indexed Tree.cpp

--- Binary Indexed Tree.cpp
+++ Binary Indexed Tree.cpp
@@ -9,28 +9,29 @@ const ll Inf = 1LL << 62;
 int ar[Max], n;
 ll BIT[Max];
 
-void update( int idx, int val ) {
-	while( idx <= n ) {
+void update(int idx, int val) {
+	while(idx <= n) {
 		BIT[idx] += val;
 		idx += idx & -idx;
 	}
 }
 
-ll query( int idx ) {
+ll query(int idx) {
 	ll ret = 0;
-	while( idx > 0 ) {
+	while(idx > 0) {
 		ret += BIT[idx];
 		idx -= idx & -idx;
-	} return ret;
+	}
+	return ret;
 }
 
-ll query( int l, int r ) {
-	return query( r ) - query( l - 1 );
+ll query(int l, int r) {
+	return query(r) - query(l - 1);
 }
 
 void build() {
-	for( int i=1; i<=n; i++ ) {
-		update( i, ar[i] );
+	for(int i = 1; i <= n; i++) {
+		update(i, ar[i]);
 	}
 }
 
@@ -43,13 +44,13 @@ int main() {
 	//cin.tie(0);
 	int q, l, r;
 	scanf("%d %d", &n, &q);
-	for( int i=1; i<=n; i++ ) {
+	for(int i = 1; i <= n; i++) {
 		scanf("%d", &ar[i]);
 	}
 	build();
-	while( q-- ) {
+	while(q--) {
 		scanf("%d %d", &l, &r);
-		printf("%lld\n", query( l, r ));
+		printf("%lld\n", query(l, r));
 	}
 	return 0;
 }
