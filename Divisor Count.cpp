a9573236c9673ad7f834e891c3590ba3dce2a041
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 15:28:51 +0600

    Update Divisor Count.cpp

--- Divisor Count.cpp
+++ Divisor Count.cpp
@@ -1,8 +1,8 @@
 ll divcnt[Max];
 
-void DivisorCount( ll n ) {
-	for( int i=1; i<=n; i++ ) {
-		for( int j=i; j<=n; j+=i ) {
+void DivisorCount(ll n) {
+	for(int i = 1; i <= n; i++) {
+		for(int j = i; j <= n; j += i) {
 			divcnt[j]++;
 		}
 	}
