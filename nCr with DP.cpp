f4b26606cb803df97526b6d061f25fd7b6418610
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 19:06:14 +0600

    Update nCr with DP.cpp

--- nCr with DP.cpp
+++ nCr with DP.cpp
@@ -1,16 +1,16 @@
 ll C[Max][Max];
 
-ll nCr( ll n, ll r ) {
-	if( n == r ) {
+ll nCr(ll n, ll r) {
+	if(n == r) {
 		return 1;
 	}
-	if( r == 1 ) {
+	if(r == 1) {
 		return n;
 	}
 	ll &ret = C[n][r];
-	if( ~ret ) {
+	if(~ret) {
 		return ret;
 	}
-	ret = nCr( n-1, r ) + nCr( n-1, r-1 );
+	ret = nCr(n - 1, r) + nCr(n - 1, r - 1);
 	return ret;
 }
