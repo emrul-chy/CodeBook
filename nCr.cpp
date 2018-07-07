c667e2b2874887560ace6b8079d24b020c869de4
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2018-07-07 19:07:07 +0600

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -11,29 +11,29 @@ const ll Inf = 1LL << 62;
 ll f[Max], inv[Max];
 
 ll BigMod(ll a, int e) {
-	if (e == -1) e = Mod - 2;
+	if(e == -1) e = Mod - 2;
 	ll r = 1;
-	while (e) {
-		if (e & 1) r *= a, r %= Mod;
+	while(e) {
+		if(e & 1) r *= a, r %= Mod;
 		a *= a, a %= Mod, e >>= 1;
 	}
 	return r;
 }
 
-ll nCr( int x, int y) {
-	if (x < 0 or y < 0 or x < y) return 0;
+ll nCr(int x, int y) {
+	if(x < 0 or y < 0 or x < y) return 0;
 	return f[x] * (inv[y] * inv[x - y] % Mod) % Mod;
 }
 
 int main() {
 	f[0] = 1;
-	for (int i = 1; i < Max; ++i) {
+	for(int i = 1; i < Max; ++i) {
 		f[i] = i * 1LL * f[i - 1] % Mod;
 	}
 	inv[Max - 1] = BigMod(f[Max - 1], -1);
-	for (int i = Max - 1; i; --i) {
+	for(int i = Max - 1; i; --i) {
 		inv[i - 1] = i * 1LL * inv[i] % Mod;
 	}
-	printf("%lld\n", nCr( 12, 2 ));
+	printf("%lld\n", nCr(12, 2));
 	return 0;
 }
