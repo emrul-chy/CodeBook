8aa10ed615f35d6ffe603de30aba055dc25f0cd5
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2019-07-04 16:23:16 +0600

    Update Z Algorithm.cpp

--- Z Algorithm.cpp
+++ Z Algorithm.cpp
@@ -9,51 +9,43 @@ const int Mod = 1e9 + 7;
 const ll Inf = 1LL << 62;
 
 int Z[Max];
-char ch[Max];
+char S[Max];
 
-void compute_z_function(const char *S, int N) {
-	int L = 0, R = 0;
-
-	for(int i = 1; i < N; ++i) {
+void compute_z_function(int n) {
+	int L, R, k;
+	L = R = 0;
+	for(int i = 1; i < n; ++i) {
 		if(i > R) {
 			L = R = i;
-
-			while(R < N && S[R - L] == S[R]) {
-				++R;
+			while(R < n && S[R - L] == S[R]) {
+				R++;
 			}
-
-			Z[i] = R - L; --R;
+			Z[i] = R - L;
+			R--;
 		}
 		else {
-			int k = i - L;
-
+			k = i - L;
 			if(Z[k] < R - i + 1) {
 				Z[i] = Z[k];
 			}
 			else {
 				L = i;
-
-				while(R < N && S[R - k] == S[R]) {
-					++R;
+				while(R < n && S[R - L] == S[R]) {
+					R++;
 				}
-
-				Z[i] = R - L; --R;
+				Z[i] = R - L;
+				R--;
 			}
 		}
 	}
 }
 
 int main() {
-#ifdef Mr_Emrul
-	freopen("inputf.in", "r", stdin);
-#endif /// Mr_Emrul
+	scanf("%s", S);
+	compute_z_function(strlen(S));
 
-	scanf("%s", ch);
-	compute_z_function(ch, strlen(ch));
-
-	for(int i = 0; i < strlen(ch); i++) {
+	for(int i = 0; i < strlen(S); i++) {
 		printf("%d ", Z[i]);
 	}
-
 	return 0;
 }
