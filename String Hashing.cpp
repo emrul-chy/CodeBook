e49840a48ac4b8caab926e6d4d4b6639d985a76b
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2019-08-13 20:15:54 +0600

    Update String Hashing.cpp

--- String Hashing.cpp
+++ String Hashing.cpp
@@ -1,34 +1,32 @@
 /*
         String Hashing
 */
-#include <bits/stdc++.h>
-using namespace std;
+typedef long long int ll;
+typedef pair <ll, ll> pll;
+const int base = 331;
 
-typedef unsigned long long int ull ;
-const int base = 31 ;
-const int Max = 1e5;
+const int Max = 2e6 + 10;
+const int Mod = 1e9 + 7;
+const ll Inf = 1LL << 62;
 
-unordered_map < ull, ull > mp ;
-
-ull pw[Max];
-ull Hash[Max];
+ll pw[Max];
+ll Hash[Max];
 
 void pre_power() {
 	pw[0] = 1 ;
 	for(int i = 1; i < Max; i++) {
-		pw[i] = pw[i - 1] * base ;
+		pw[i] = (pw[i - 1] * base) % Mod;
 	}
 }
 
 void Hashing(string str, int len) {
-	ull hash_val = 0;
+	ll hash_val = 0;
 	for(int i = 0; i < len; i++) {
-		hash_val = hash_val * base + str[i];
+		hash_val = (hash_val * base + str[i]) % Mod;
 		Hash[i + 1] = hash_val;
 	}
 }
 
-ull SubstringHash(int l, int r) {
-	return Hash[r] - Hash[l - 1] * pw[r - l + 1];
+ll SubstringHash(int l, int r) {
+	return (Hash[r] - (Hash[l - 1] * pw[r - l + 1]) % Mod + Mod) % Mod;
 }
-
