8a3838bbf47a82f0c9a9560187b92bd91044cdb5
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2019-10-04 22:44:18 +0600

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -1,13 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long int ll;
-typedef pair <ll, ll> pll;
-
-const int Max = 2e6 + 10;
-const int Mod = 1e9 + 7;
-const ll Inf = 1LL << 62;
-
 ll f[Max], inv[Max];
 
 ll BigMod(ll a, int e) {
@@ -25,7 +15,7 @@ ll nCr(int x, int y) {
 	return f[x] * (inv[y] * inv[x - y] % Mod) % Mod;
 }
 
-int main() {
+void init() {
 	f[0] = 1;
 	for(int i = 1; i < Max; ++i) {
 		f[i] = i * 1LL * f[i - 1] % Mod;
@@ -34,6 +24,4 @@ int main() {
 	for(int i = Max - 1; i; --i) {
 		inv[i - 1] = i * 1LL * inv[i] % Mod;
 	}
-	printf("%lld\n", nCr(12, 2));
-	return 0;
 }
